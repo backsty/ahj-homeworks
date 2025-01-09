324f8db7da7c9c17f333f4fe917df78e486bb7b2
backsty <shalamovartem6@mail.ru>
2025-01-09 22:13:14 +0700

    feat(dnd): fixed path

--- dnd/webpack.prod.cjs
+++ dnd/webpack.prod.cjs
@@ -1,14 +1,15 @@
 const { merge } = require('webpack-merge');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-const baseConfig = require('./webpack.config.js');
+const baseConfig = require('./webpack.config.cjs');
 
+const ASSET_PATH = process.env.ASSET_PATH || '/ahj-homeworks/dnd/';
 
 module.exports = merge(baseConfig, {
   mode: 'production',
   devtool: 'source-map',
   output: {
-    publicPath: '/dnd/',
+    publicPath: ASSET_PATH,
   },
   optimization: {
     minimize: true,
