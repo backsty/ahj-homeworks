e9cb409ab0b43d815fdc08f2fa1d7b5a0cd73857
backsty <shalamovartem6@mail.ru>
2025-02-07 01:22:27 +0700

    feat(workers): add .env

--- workers/backend/src/config.js
+++ workers/backend/src/config.js
@@ -6,5 +6,6 @@ export default {
         credentials: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
         allowedHeaders: ['Content-Type', 'Authorization']
-    }
+    },
+    isProduction: process.env.NODE_ENV === 'production'
 }
