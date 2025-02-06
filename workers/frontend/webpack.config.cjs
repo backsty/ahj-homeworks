7c9fcf079b1c39a5de96f81cae27e73a6ccbc5c1
backsty <shalamovartem6@mail.ru>
2025-02-07 02:59:12 +0700

    fix(workers): update publicPath and hompage path

--- workers/frontend/webpack.config.cjs
+++ workers/frontend/webpack.config.cjs
@@ -13,7 +13,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].js',
-    publicPath: '/',
+    publicPath: '/ahj-homeworks/workers/frontend/',
   },
   resolve: {
     extensions: ['.js', '.jsx', '.json'],
